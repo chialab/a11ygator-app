86c7bff4d8be40ea6cf8dc267fb317852f787676
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2018-09-20 18:12:50 +0200

    feat: first working flux

--- packages/ws/index.js
+++ packages/ws/index.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const chia11y = require('./chia11y.js');
 const app = express()
-const chia11y = require('./chia11y');
 
 const PORT = process.env.PORT || 9000;
 
 app.use('/screenshots', express.static(__dirname + '/screenshots'));
-app.use('/app', express.static(__dirname + '/public'));
+app.use('/app', express.static(__dirname));
+
+app.all('/', async function (req, res) {
+    const url = req.query && req.query.url;
+    if (!url) {
+        res.status(400);
+        return res.send('Missing url');
+    }
+    const options = req.body || {};
+    if (options.url) {
+        delete options.url;
+    }
+    const result = await chia11y(url, options);
+    console.log(result)
+    res.send(result)
+});
+
+app.get('/app', (req, res) => {
+    res.sendFile('public/index.html', { root : __dirname});
+});
 
 app.listen(PORT);
--- packages/ws/public/index.js
+++ packages/ws/public/index.js
@@ -1,3 +1,22 @@
-sendData = function() {
-    alert('sisisi')
+sendData = async function (ev) {
+    ev.preventDefault();
+
+    const elements = document.querySelector("#site-form").elements;
+    const data = {};
+    for (let i = 0; i < elements.length; i++) {
+        const item = elements.item(i);
+        data[item.name] = item.value;
+    }
+
+    const options = data;
+    const url = data.url;
+    delete options.url;
+
+    document.querySelector('.result-container').innerHTML = 'Loading';
+
+    const res = await fetch(`${document.location.origin}/?url=${url}`);
+    res.json()
+        .then((json) => {
+            document.querySelector('.result-container').innerHTML = JSON.stringify(json);
+        })
 }
