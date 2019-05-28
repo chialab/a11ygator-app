7948505abe0f874308e37a1de27b2cb0678b27e7
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-05-28 10:15:07 +0200

    chore: cleanup

--- server.js
+++
@@ -1,7 +0,0 @@
-const { app } = require('./src/app.js');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`A11ygator 🐊 is waiting for you on http://localhost:${PORT}`);
-});
